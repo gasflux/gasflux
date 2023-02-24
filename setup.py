a9838f91cafe31cfef1c145ce00a86f3dbad7d3b
pipari <jamie.mcquilkin@postgrad.manchester.ac.uk>
2023-02-24 16:51:08 +0000

    first commit - add flux.py and various linting/setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='ghgflux',
+    version='0.0.1',
+    author='pipari',
+    author_email='jamie.mcquilkin@postgrad.manchester.ac.uk',
+    url='',
+    license='',
+    packages=['ghgflux'],
+    description='Calculating gas fluxes from UAV data',
+    install_requires=[
+        'python>=3.10.0',
+    ],
+)
