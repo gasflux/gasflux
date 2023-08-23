fce267169610fb53706de76a2df41887b1272705
jamie <jamie.mcquilkin@postgrad.manchester.ac.uk>
2023-08-23 12:21:38 +0100

    change name to gasflux and fix small typerror

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="ghgflux",
+    name="gasflux",
     version="0.0.1",
     author="Jamie McQuilkin",
     author_email="jamie.mcquilkin@postgrad.manchester.ac.uk",
